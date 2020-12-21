2f93f575682f91f16f92af89b03db4ef736e3152
Mad <github@typ03.de>
2020-12-21 14:37:29 +0100

    bump version

--- Source/AssortedAdjustments/mod_info.js
+++ Source/AssortedAdjustments/mod_info.js
@@ -1,7 +1,7 @@
 {
 	Id : "Mad.AssortedAdjustments",
 	Name : "AssortedAdjustments",
-    Version: "1.9-001R",
+    Version: "1.9-002R",
 	Description : "Various little tweaks to adjust the game to your liking.",
 	Requires : { Id: "Modnix", Min: "2.0" },
 	ConfigType : "AssortedAdjustments.Settings"
