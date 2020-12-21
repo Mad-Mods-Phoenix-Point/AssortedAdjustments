689cb8560764fec440ba74d35ade6eceb1f2ee1c
Mad <github@typ03.de>
2020-12-21 14:35:47 +0100

    add return fire adjustments and adapted spillxptosp to work with YOE

---
+++ Source/AssortedAdjustments/Patches/EnableExperienceToSkillpointConversion.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using Harmony;
+using PhoenixPoint.Common.Entities;
+using PhoenixPoint.Common.Entities.Characters;
+using PhoenixPoint.Common.Levels.Missions;
+using PhoenixPoint.Geoscape.Entities;
+using PhoenixPoint.Tactical.Entities;
+using PhoenixPoint.Tactical.Levels;
+using PhoenixPoint.Tactical.View.ViewControllers;
+using PhoenixPoint.Tactical.View.ViewModules;
+
+namespace AssortedAdjustments.Patches
+{
+    internal static class ExperienceToSkillpointConversion
+    {
+        private static bool captureExperience = false;
+        private static Dictionary<LevelProgression, int> missionExperience;
+        private static Dictionary<GeoTacUnitId, int> convertedSkillpoints;
+
+
+
+
+        [HarmonyPatch(typeof(TacticalFaction), "GiveExperienceForObjectives")]
+        public static class TacticalFaction_GiveExperienceForObjectives_Patch
+        {
+            public static bool Prepare()
+            {
+                return AssortedAdjustments.Settings.EnableExperienceToSkillpointConversion;
+            }
+
+            public static void Prefix(TacticalFaction __instance)
+            {
+                try
+                {
+                    if (__instance?.ParticipantKind != TacMissionParticipant.Player)
+                    {
+                        return;
+                    }
+
+                    Logger.Debug($"[TacticalFaction_GiveExperienceForObjectives_PREFIX] Handling faction: {__instance.TacticalFactionDef.GetName()}.");
+
+                    missionExperience = new Dictionary<LevelProgression, int>(16);
+                    convertedSkillpoints = new Dictionary<GeoTacUnitId, int>(16);
+                    captureExperience = true;
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+
+            public static void Postfix(TacticalFaction __instance)
+            {
+                try
+                {
+                    if (missionExperience == null)
+                    {
+                        return;
+                    }
+                    captureExperience = false;
+
+                    float xpConversionRate = __instance.TacticalLevel?.Difficulty?.ExpConvertedToSkillpoints ?? AssortedAdjustments.Settings.XPtoSPConversionRate;
+                    float xpConversionMultiplier = AssortedAdjustments.Settings.XPtoSPConversionMultiplier;
+
+                    foreach (TacticalActor actor in __instance.TacticalActors)
+                    {
+                        LevelProgression levelProgression = actor?.LevelProgression;
+                        if (levelProgression == null || !actor.IsAlive || !missionExperience.TryGetValue(levelProgression, out int xpMission)) continue;
+                        int xpEarned = levelProgression.Experience - levelProgression.ExperienceReference;
+                        int xpToConvert = xpMission - xpEarned;
+                        int skillpoints = Math.Max(0, (int)Math.Floor(xpToConvert * xpConversionRate * xpConversionMultiplier));
+                        if (skillpoints > 0)
+                        {
+                            convertedSkillpoints[actor.GeoUnitId] = skillpoints;
+                            Logger.Info($"[TacticalFaction_GiveExperienceForObjectives_POSTFIX] xpConversionRate: {xpConversionRate}");
+                            Logger.Info($"[TacticalFaction_GiveExperienceForObjectives_POSTFIX] xpConversionMultiplier: {xpConversionMultiplier}");
+                            Logger.Info(String.Format("[TacticalFaction_GiveExperienceForObjectives_POSTFIX] (Tac) {0} earned a total of {1} XP({2},{3}) and had the overflow of {3} converted into {4} SP", actor.GetDisplayName(), xpMission, xpEarned, xpToConvert, skillpoints));
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+
+
+
+        [HarmonyPatch(typeof(LevelProgression), "AddExperience")]
+        public static class LevelProgression_AddExperience_Patch
+        {
+            public static bool Prepare()
+            {
+                return AssortedAdjustments.Settings.EnableExperienceToSkillpointConversion;
+            }
+
+            public static void Postfix(LevelProgression __instance, int experience)
+            {
+                try
+                {
+                    if (!captureExperience || missionExperience == null)
+                    {
+                        return;
+                    }
+                    Logger.Debug($"[LevelProgression_AddExperience_POSTFIX] Capture experience({experience}).");
+
+                    missionExperience.TryGetValue(__instance, out int xpMission);
+                    missionExperience[__instance] = xpMission + experience;
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+
+
+
+        [HarmonyPatch(typeof(UIModuleBattleSummary), "SetProgressSlider")]
+        public static class UIModuleBattleSummary_SetProgressSlider_Patch
+        {
+            public static bool Prepare()
+            {
+                return AssortedAdjustments.Settings.EnableExperienceToSkillpointConversion;
+            }
+
+            public static void Postfix(UIModuleBattleSummary __instance, SoldierResultElement soldierResultElement)
+            {
+                try
+                {
+                    TacticalActor actor = soldierResultElement.Actor;
+                    LevelProgression levelProgression = actor?.LevelProgression;
+                    if (levelProgression == null || !actor.IsAlive || levelProgression.Experience != levelProgression.ExperienceReference)
+                    {
+                        return;
+                    }
+
+                    if (!convertedSkillpoints.TryGetValue(actor.GeoUnitId, out int skillpoints))
+                    {
+                        return;
+                    }
+
+                    soldierResultElement.EarnedExperience.text = String.Format("SP +{0}", skillpoints);
+                    soldierResultElement.EarnedExperience.color = soldierResultElement.DefaultStatus.TextColor;
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+
+
+
+        [HarmonyPatch(typeof(GeoCharacter), "ApllyTacticalResult")] // sic!
+        public static class GeoCharacter_ApllyTacticalResult_Patch
+        {
+            public static bool Prepare()
+            {
+                return AssortedAdjustments.Settings.EnableExperienceToSkillpointConversion;
+            }
+
+            public static void Postfix(GeoCharacter __instance, TacActorUnitResult result)
+            {
+                try
+                {
+                    if (convertedSkillpoints == null || !convertedSkillpoints.TryGetValue(result.GeoUnitId, out int skillpoints))
+                    {
+                        return;
+                    }
+
+                    if (skillpoints > 0)
+                    {
+                        Logger.Info(String.Format("[GeoCharacter_ApllyTacticalResult_POSTFIX] (Geo) {0} has {1} SP and will earn additional {2} SP from XP conversion", __instance.GetName(), result.CharacterProgression.SkillPoints, skillpoints));
+
+                        __instance.Progression.SkillPoints += skillpoints;
+                        convertedSkillpoints.Remove(result.GeoUnitId);
+                        if (convertedSkillpoints.Count <= 0)
+                        {
+                            convertedSkillpoints = null;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+    }
+}
---
+++ Source/AssortedAdjustments/Patches/EnableReturnFireAdjustments.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Base.Utils.Maths;
+using Harmony;
+using PhoenixPoint.Tactical.Entities;
+using PhoenixPoint.Tactical.Entities.Abilities;
+using PhoenixPoint.Tactical.Entities.Weapons;
+using PhoenixPoint.Tactical.Levels;
+using UnityEngine;
+
+namespace AssortedAdjustments.Patches
+{
+    internal static class ReturnFireAdjustments
+    {
+        internal static float reactionAngleCos = (float) Math.Cos(AssortedAdjustments.Settings.ReturnFireAngle * Math.PI / 180d / 2d);
+        internal static Dictionary<TacticalActor, int> returnFireCounter = new Dictionary<TacticalActor, int>();
+
+
+
+        // Helper
+        private static bool CanReturnFireFromAngle(TacticalActor shooter, TacticalActorBase target, float reactionAngleCos)
+        {
+            // Turrets cannot be flanked
+            if (target.IsMetallic)
+            {
+                return true;
+            }
+
+            if (reactionAngleCos > 0.99)
+            {
+                return true;
+            }
+
+            Vector3 targetForward = target.transform.TransformDirection(Vector3.forward);
+            Vector3 targetToShooter = (shooter.Pos - target.Pos).normalized;
+            float angleCos = Vector3.Dot(targetForward, targetToShooter);
+
+            return Utl.GreaterThanOrEqualTo(angleCos, reactionAngleCos);
+        }
+
+        private static bool HasReachedReturnFireLimit(TacticalActor target)
+        {
+            // Turrets have no limit
+            if (target.IsMetallic)
+            {
+                return false;
+            }
+
+            int returnFireLimit = AssortedAdjustments.Settings.ReturnFireLimit;
+            if (returnFireLimit > 0)
+            {
+                returnFireCounter.TryGetValue(target, out var currentCount);
+                Logger.Debug($"[ReturnFireAdjustments_HasReachedReturnFireLimit] {target.DisplayName}, returnFireCount: {currentCount}");
+                return currentCount >= returnFireLimit;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+
+        // Patches
+        [HarmonyPatch(typeof(TacticalFaction), "PlayTurnCrt")]
+        public static class TacticalFaction_PlayTurnCrt_Patch
+        {
+            public static bool Prepare()
+            {
+                return AssortedAdjustments.Settings.EnableReturnFireAdjustments;
+            }
+
+            public static void Prefix(TacticalFaction __instance)
+            {
+                try
+                {
+                    //Logger.Debug($"[TacticalFaction_PlayTurnCrt_PREFIX] Resetting return fire count for all actors of faction ({__instance.TacticalFactionDef.GetName()}).");
+                    /*
+                    List<TacticalActor> allFactionActors = __instance.TacticalActors.ToList();
+                    foreach (TacticalActor actor in allFactionActors)
+                    {
+                        if (returnFireCounter.ContainsKey(actor))
+                        {
+                            returnFireCounter[actor] = 0;
+                        }
+                        else
+                        {
+                            returnFireCounter.Add(actor, 0);
+                        }
+                    }
+                    */
+
+                    // Only track enemies of the faction starting its turn
+                    returnFireCounter = returnFireCounter
+                        .Where(tacticalActor => tacticalActor.Key.TacticalFaction != __instance)
+                        .ToDictionary(tacticalActor => tacticalActor.Key, tacticalActor => tacticalActor.Value);
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+
+
+
+        [HarmonyPatch(typeof(TacticalLevelController), "FireWeaponAtTargetCrt")]
+        public static class TacticalLevelController_FireWeaponAtTargetCrt_Patch
+        {
+            public static bool Prepare()
+            {
+                return AssortedAdjustments.Settings.EnableReturnFireAdjustments;
+            }
+
+            public static void Prefix(TacticalLevelController __instance, Weapon weapon, TacticalAbilityTarget abilityTarget)
+            {
+                try
+                {
+                    if (abilityTarget.AttackType == AttackType.ReturnFire)
+                    {
+                        TacticalActor tacticalActor = weapon.TacticalActor;
+                        returnFireCounter.TryGetValue(tacticalActor, out var currentCount);
+                        returnFireCounter[tacticalActor] = currentCount + 1;
+
+                        Logger.Debug($"[TacticalLevelController_FireWeaponAtTargetCrt_PREFIX] Actor: {tacticalActor.DisplayName}, returnFireCount: {returnFireCounter[tacticalActor]}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+
+
+
+        [HarmonyPatch(typeof(TacticalLevelController), "GetReturnFireAbilities")]
+        public static class TacticalLevelController_GetReturnFireAbilities_Patch
+        {
+            public static bool Prepare()
+            {
+                return AssortedAdjustments.Settings.EnableReturnFireAdjustments;
+            }
+
+            public static void Postfix(TacticalLevelController __instance, ref List<ReturnFireAbility> __result, TacticalActor shooter, Weapon weapon, TacticalAbilityTarget target)
+            {
+                try
+                {
+                    // No potential return fire from original method
+                    if (__result == null || __result.Count == 0)
+                    {
+                        Logger.Debug($"[TacticalLevelController_GetReturnFireAbilities_POSTFIX] Original method already canceled return fire.");
+                        return;
+                    }
+
+                    // Double check
+                    WeaponDef weaponDef = weapon?.WeaponDef;
+                    if (target.AttackType == AttackType.ReturnFire || target.AttackType == AttackType.Overwatch || target.AttackType == AttackType.Synced || target.AttackType == AttackType.ZoneControl || (weaponDef != null && weaponDef.NoReturnFireFromTargets))
+                    {
+                        return;
+                    }
+
+
+
+                    Logger.Debug($"[TacticalLevelController_GetReturnFireAbilities_POSTFIX] Disabling return fire if target is shot from outside its reaction angle ({AssortedAdjustments.Settings.ReturnFireAngle} degrees) or has reached its return fire limit ({AssortedAdjustments.Settings.ReturnFireLimit}).");
+
+                    List<ReturnFireAbility> returnFireAbilities = __result;
+
+                    // Reverse iteration to be able to modify list directly
+                    for (int i = returnFireAbilities.Count - 1; i >= 0; i--)
+                    {
+                        TacticalActor tacticalActor = returnFireAbilities[i].TacticalActor;
+
+                        Logger.Debug($"[TacticalLevelController_GetReturnFireAbilities_POSTFIX] shooter: {shooter.DisplayName}");
+                        Logger.Debug($"[TacticalLevelController_GetReturnFireAbilities_POSTFIX] target: {tacticalActor.DisplayName}");
+
+                        // Check flanked
+                        if (!CanReturnFireFromAngle(shooter, tacticalActor, reactionAngleCos))
+                        {
+                            Logger.Debug($"[TacticalLevelController_GetReturnFireAbilities_POSTFIX] {tacticalActor.DisplayName} is flanked and cannot return fire.");
+                            returnFireAbilities.RemoveAt(i);
+                            continue;
+                        }
+                        else
+                        {
+                            Logger.Debug($"[TacticalLevelController_GetReturnFireAbilities_POSTFIX] {tacticalActor.DisplayName} isn't flanked will still return fire.");
+                        }
+
+                        // Check shot limit
+                        if (HasReachedReturnFireLimit(tacticalActor))
+                        {
+                            Logger.Debug($"[TacticalLevelController_GetReturnFireAbilities_POSTFIX] {tacticalActor.DisplayName} has reached its return fire limit and cannot return fire.");
+                            returnFireAbilities.RemoveAt(i);
+                            continue;
+                        }
+                        else
+                        {
+                            Logger.Debug($"[TacticalLevelController_GetReturnFireAbilities_POSTFIX] {tacticalActor.DisplayName} hasn't reached its return fire limit and will still return fire.");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+    }
+}
