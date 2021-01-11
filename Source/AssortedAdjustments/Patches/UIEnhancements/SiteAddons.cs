4bf8f086bc28dd22b7160555e5730cbf953d6c50
Mad <github@typ03.de>
2021-01-11 19:57:32 +0100

    refactor site addons code

---
+++ Source/AssortedAdjustments/Patches/UIEnhancements/SiteAddons.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Harmony;
+using PhoenixPoint.Geoscape.View;
+using PhoenixPoint.Geoscape.Entities;
+using PhoenixPoint.Common.Core;
+
+namespace AssortedAdjustments.Patches.UIEnhancements
+{
+    internal static class SiteAddons
+    {
+        [HarmonyPatch(typeof(GeoSiteVisualsController), "RefreshSiteVisuals")]
+        public static class GeoSiteVisualsController_RefreshSiteVisuals_Patch
+        {
+            public static bool Prepare()
+            {
+                return AssortedAdjustments.Settings.HideSiteAddons;
+            }
+
+            public static void Postfix(GeoSiteVisualsController __instance, GeoSite site)
+            {
+                try
+                {
+                    if (site.Type != GeoSiteType.Haven)
+                    {
+                        return;
+                    }
+
+                    // See lines 167ff in original method
+                    if (__instance.SiteSpecialAddonContainer.transform.childCount > 0)
+                    {
+                        Logger.Debug($"[GeoSiteVisualsController_RefreshSiteVisuals_POSTFIX] Hiding special addons of site: {site.Name}");
+                        __instance.SiteSpecialAddonContainer.SetActive(false);
+                    }
+                    if (__instance.SiteUniqueAddonContainer.transform.childCount > 0)
+                    {
+                        Logger.Debug($"[GeoSiteVisualsController_RefreshSiteVisuals_POSTFIX] Hiding unique addons of site: {site.Name}");
+                        __instance.SiteUniqueAddonContainer.SetActive(false);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+    }
+}
