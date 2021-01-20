82e3589f673a8907f10a7054bba698b47f80f8e4
Mad <github@typ03.de>
2021-01-20 09:03:34 +0100

    add option to center on excavation complete

---
+++ Source/AssortedAdjustments/Patches/CenterOnExcavationComplete.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Harmony;
+using PhoenixPoint.Geoscape.Levels.Factions.Archeology;
+using PhoenixPoint.Geoscape.View;
+using PhoenixPoint.Geoscape.View.ViewStates;
+
+namespace AssortedAdjustments.Patches
+{
+    [HarmonyPatch(typeof(UIStateVehicleSelected), "OnVehicleSiteExcavated")]
+    public static class UIStateVehicleSelected_OnVehicleSiteExcavated_Patch
+    {
+        public static bool Prepare()
+        {
+            return AssortedAdjustments.Settings.CenterOnExcavationComplete;
+        }
+
+        public static void Postfix(UIStateVehicleSelected __instance, SiteExcavationState excavation)
+        {
+            try
+            {
+                if (excavation.Site != null)
+                {
+                    Logger.Info($"[UIStateVehicleSelected_OnVehicleSiteExcavated_POSTFIX] Chase excavation site.");
+
+                    GeoscapeViewContext ___Context = (GeoscapeViewContext)AccessTools.Property(typeof(GeoscapeViewState), "Context").GetValue(__instance, null);
+                    ___Context.View.ChaseTarget(excavation.Site, false);
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+            }
+        }
+    }
+}
