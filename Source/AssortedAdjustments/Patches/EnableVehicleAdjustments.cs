1571020462db22b76420686e25257921788289f2
Mad <github@typ03.de>
2021-01-20 09:02:56 +0100

    Add option to change verhicle ranges

--- Source/AssortedAdjustments/Patches/EnableVehicleAdjustments.cs
+++ Source/AssortedAdjustments/Patches/EnableVehicleAdjustments.cs
@@ -19,24 +19,28 @@ namespace AssortedAdjustments.Patches
                 {
                     gvDef.BaseStats.Speed.Value = AssortedAdjustments.Settings.AircraftBlimpSpeed;
                     gvDef.BaseStats.SpaceForUnits = AssortedAdjustments.Settings.AircraftBlimpSpace;
+                    gvDef.BaseStats.MaximumRange.Value = AssortedAdjustments.Settings.AircraftBlimpRange;
                 }
                 else if (gvDef.name.Contains("Thunderbird"))
                 {
                     gvDef.BaseStats.Speed.Value = AssortedAdjustments.Settings.AircraftThunderbirdSpeed;
                     gvDef.BaseStats.SpaceForUnits = AssortedAdjustments.Settings.AircraftThunderbirdSpace;
+                    gvDef.BaseStats.MaximumRange.Value = AssortedAdjustments.Settings.AircraftThunderbirdRange;
                 }
                 else if (gvDef.name.Contains("Manticore"))
                 {
                     gvDef.BaseStats.Speed.Value = AssortedAdjustments.Settings.AircraftManticoreSpeed;
                     gvDef.BaseStats.SpaceForUnits = AssortedAdjustments.Settings.AircraftManticoreSpace;
+                    gvDef.BaseStats.MaximumRange.Value = AssortedAdjustments.Settings.AircraftManticoreRange;
                 }
                 else if (gvDef.name.Contains("Helios"))
                 {
                     gvDef.BaseStats.Speed.Value = AssortedAdjustments.Settings.AircraftHeliosSpeed;
                     gvDef.BaseStats.SpaceForUnits = AssortedAdjustments.Settings.AircraftHeliosSpace;
+                    gvDef.BaseStats.MaximumRange.Value = AssortedAdjustments.Settings.AircraftHeliosRange;
                 }
 
-                Logger.Info($"[VehicleAdjustments_Apply] gvDef: {gvDef.name}, GUID: {gvDef.Guid}, Speed: {gvDef.BaseStats.Speed.Value}, SpaceForUnits: {gvDef.BaseStats.SpaceForUnits}");
+                Logger.Info($"[VehicleAdjustments_Apply] gvDef: {gvDef.name}, GUID: {gvDef.Guid}, Speed: {gvDef.BaseStats.Speed.Value}, SpaceForUnits: {gvDef.BaseStats.SpaceForUnits}, MaximumRange: {gvDef.BaseStats.MaximumRange.Value}");
             }
         }
     }
