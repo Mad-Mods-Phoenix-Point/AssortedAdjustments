c2059f72d98213ca0e9a0710a4cf69f7cc929a35
Mad <github@typ03.de>
2021-01-31 11:13:35 +0100

    change versioning style

--- Source/AssortedAdjustments/mod_info.js
+++ Source/AssortedAdjustments/mod_info.js
@@ -1,7 +1,7 @@
 {
 	Id : "Mad.AssortedAdjustments",
 	Name : "AssortedAdjustments",
-    Version: "1.9.0.11",
+    Version: "1.9.1.1",
 	Description : "Various little tweaks to adjust the game to your liking.",
     Lang: "en",
     Author: "Mad",
