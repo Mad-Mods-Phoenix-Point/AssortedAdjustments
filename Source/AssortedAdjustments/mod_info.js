de201fc74c8cacf30479ca9a3b95fb37a41f441f
mad <github@mad-season.de>
2021-02-25 09:24:22 +0100

    exp changelog, bump version

--- Source/AssortedAdjustments/mod_info.js
+++ Source/AssortedAdjustments/mod_info.js
@@ -1,7 +1,7 @@
 {
 	Id : "Mad.AssortedAdjustments",
 	Name : "AssortedAdjustments",
-    Version: "1.10.0.3",
+    Version: "1.10.0.4",
 	Description : "Various little tweaks to adjust the game to your liking.",
     Lang: "en",
     Author: "Mad",
