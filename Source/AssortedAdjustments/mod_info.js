38dd2a063135df6d5811995332211c9226cde720
mad <github@mad-season.de>
2021-02-25 09:20:43 +0100

    make mod_info.js an embedded resource

--- Source/AssortedAdjustments/mod_info.js
+++ Source/AssortedAdjustments/mod_info.js
@@ -5,6 +5,10 @@
 	Description : "Various little tweaks to adjust the game to your liking.",
     Lang: "en",
     Author: "Mad",
+	Url: {
+		"GitHub": "https://github.com/Mad-Mods-Phoenix-Point/AssortedAdjustments",
+		"Nexus": "https://www.nexusmods.com/phoenixpoint/mods/53"
+	},
     Contact: "https://github.com/mad-mods-phoenix-point",
 	Requires : { Id: "Modnix", Min: "2.0" },
 	ConfigType : "AssortedAdjustments.Settings"
